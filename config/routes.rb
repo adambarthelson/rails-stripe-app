6194142516838303056fc8dee28c75e7fd0c43a3
benjamintush <benja.tush@gmail.com>
2013-05-03 19:02:34 +0300

    remaining on the glider part

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 RailsStripeApp::Application.routes.draw do
+
+  
+  match '/about',         to: 'static_pages#about'
+  match '/contact',       to: 'static_pages#contact'
+  match '/help',          to: 'static_pages#help'
+
   mount StripeEvent::Engine => '/stripe'
   get "content/gold"
   get "content/silver"
